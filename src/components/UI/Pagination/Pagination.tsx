74100451cd53ff55792a18a1f4068be1907bb7a5
Jakub Parol <kubciop33@gmail.com>
2023-09-17 21:34:06 +0200

    bf: fix pagination

--- src/components/UI/Pagination/Pagination.tsx
+++ src/components/UI/Pagination/Pagination.tsx
@@ -1,9 +1,10 @@
 import clsx from "clsx";
 import { type ComponentPropsWithoutRef, type FC } from "react";
+import { type Route } from "next";
 import { ActiveLink } from "../ActiveLink/ActiveLink";
 import { ProjectUrls } from "@/const";
 
-export interface PaginationProps extends ComponentPropsWithoutRef<"nav"> {
+export interface PaginationProps extends ComponentPropsWithoutRef<"ul"> {
 	numOfPages: number;
 }
 
@@ -11,16 +12,16 @@ export const Pagination: FC<PaginationProps> = (props) => {
 	const { numOfPages, className, ...rest } = props;
 
 	return (
-		<nav {...rest} className={clsx(className)}>
-			<ul aria-label="pagination" className="mt-4 flex items-center justify-center gap-8">
-				{Array.from({ length: numOfPages }, (_, i) => i + 1).map((page) => (
-					<li key={page}>
-						{/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
-						{/* @ts-ignore */}
-						<ActiveLink href={`${ProjectUrls.Products}/${page}`} label={page.toString()} />
-					</li>
-				))}
-			</ul>
-		</nav>
+		<ul
+			{...rest}
+			aria-label="pagination"
+			className={clsx("mt-4 flex items-center justify-center gap-8", className)}
+		>
+			{Array.from({ length: numOfPages }, (_, i) => i + 1).map((page) => (
+				<li key={page}>
+					<ActiveLink href={`${ProjectUrls.Products}/${page}` as Route} label={page.toString()} />
+				</li>
+			))}
+		</ul>
 	);
 };
