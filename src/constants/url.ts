1b502e8dd950f1c0455e444f232acaf8272357b0
kubaparol <kubciop33@gmail.com>
2024-03-18 21:59:59 +0100

    ft: add urls

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -4,6 +4,7 @@ export const ProjectUrls = Object.freeze({
 	home: "/",
 	products: "/products",
 	cart: "/cart",
+	sizeGuide: "/size-guide",
 	product: (id: string) => `/product/${id}` as Route,
 	category: (slug: string) => `/category/${slug}` as Route,
 	collection: (slug: string) => `/collection/${slug}` as Route,
